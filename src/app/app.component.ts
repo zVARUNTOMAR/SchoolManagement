e1c0cf954fb83e6bc5fdfce93994d03a290b99de
zvaruntomar <vrnkmr119@gmail.com>
2022-08-29 16:13:53 +0530

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'schoolmanagement';
+  client_env_name: string = environment.env_name;
+  server_env_name: string = '';
 }
